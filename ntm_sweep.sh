15411fe8d36a00f58a1ceb14fd6c4d173215c0f0
Fchaubard <fchaubard@gmail.com>
2025-01-08 08:58:21 -0800

    Update ntm_sweep.sh

--- ntm_sweep.sh
+++ ntm_sweep.sh
@@ -8,24 +8,47 @@
 # 2) Launch new screen sessions for each hyperparam combination
 #
 # Usage:
-#   chmod +x ntm_sweep.sh
-#   ./ntm_sweep.sh
+#   chmod +x run_sweep_screens.sh
+#   ./run_sweep_screens.sh
 ##############################################################################
 
 # 1) Kill existing screens with prefix "NTM_"
 echo "[INFO] Killing existing screen sessions named 'NTM_*'..."
-screen -ls | grep '\.NTM_' | awk '{print $1}' | xargs -I{} screen -S {} -X quit #2>/dev/null
+screen -ls | grep '\.NTM_' | awk '{print $1}' | xargs -I{} screen -S {} -X quit # 2>/dev/null
 
-# 2) Hyperparameter values to sweep
-TASKS=("copy" "repeat_copy" "associative_recall")
-HIDDEN_SIZES=(64 128 1280)
-MEMORY_SIZES=(64 128 1280)
-HEAD_SIZES=(32 64 6400)
-LEARNING_RATES=(0.001 0.0001)
+echo "[INFO] Starting to launch screens.."
+# 2) Define hyperparameter values
+TASKS=("copy" "repeat_copy" "associative_recall" "add" "sub" "mul" "div" "fib" "factorial")
+
+# ARCHITECTURES=("ntm" "dnc" "tra")
+MEZO_LAYERWISE=("false" "true")
+
+###
+# HIDDEN_SIZES=(64 128)
+# MEMORY_SIZES=(64 128)
+# HEAD_SIZES=(32 64)
+# LEARNING_RATES=(0.001 0.0001)
+# EPSILONS=(0.01 0.001 0.0001)
+# BATCH_SIZES=(16 32)
+
+ARCHITECTURES=("ntm")
+
+HIDDEN_SIZES=(64)
+MEMORY_SIZES=(64)
+HEAD_SIZES=(32)
+LEARNING_RATES=(0.001)
+EPSILONS=(0.01)
+BATCH_SIZES=(16)
+
+
+MEZO_LAYERWISE=("false")
+
+# We can toggle "adam" vs "mezo" with a single array:
 OPTIMIZERS=("adam" "mezo")
+OPTIMIZERS=("mezo")
+
 
 MAX_ITERS=1000000
-BATCH_SIZE=16
 SEQ_LEN=10
 LOG_INTERVAL=500
 
@@ -34,72 +57,83 @@ RUN_PREFIX="NTM_"
 
 # 3) Sweep over each config
 for TASK in "${TASKS[@]}"; do
-  for HS in "${HIDDEN_SIZES[@]}"; do
-    for MEM in "${MEMORY_SIZES[@]}"; do
-      for HEAD_SZ in "${HEAD_SIZES[@]}"; do
-        for LR in "${LEARNING_RATES[@]}"; do
-          for OPT in "${OPTIMIZERS[@]}"; do
+  for ARCH in "${ARCHITECTURES[@]}"; do
+    for HS in "${HIDDEN_SIZES[@]}"; do
+      for MEM in "${MEMORY_SIZES[@]}"; do
+        for HEAD_SZ in "${HEAD_SIZES[@]}"; do
+          for BS in "${BATCH_SIZES[@]}"; do
+            for LR in "${LEARNING_RATES[@]}"; do
+              for OPT in "${OPTIMIZERS[@]}"; do
 
-            if [ "$OPT" = "mezo" ]; then
-              # For MeZO, we can optionally sweep mezo_layerwise as well
-              for MLW in "false" "true"; do
-                RUN_NAME="${RUN_PREFIX}${TASK}_hs${HS}_mem${MEM}_head${HEAD_SZ}_lr${LR}_OPTmezo_LW${MLW}"
-                echo "[INFO] Launching screen session: $RUN_NAME"
+                if [ "$OPT" = "mezo" ]; then
+                  # MeZO => we also sweep EPSILON & mezo_layerwise
+                  for EPS in "${EPSILONS[@]}"; do
+                    for MLW in "${MEZO_LAYERWISE[@]}"; do
 
-                # Launch the run in a new screen session
-                screen -dmS "$RUN_NAME" bash -c "
-                  echo '[INFO] Starting run: $RUN_NAME';
-                  export WANDB_RUN_NAME=$RUN_NAME;
-                  python ntm_with_modern_training_runs.py \
-                    --task $TASK \
-                    --seq_len $SEQ_LEN \
-                    --hidden_size $HS \
-                    --memory_size $MEM \
-                    --cosine_lr \
-                    --head_size $HEAD_SZ \
-                    --num_heads 1 \
-                    --batch_size $BATCH_SIZE \
-                    --max_iters $MAX_ITERS \
-                    --log_interval $LOG_INTERVAL \
-                    --learning_rate $LR \
-                    --optimizer mezo \
-                    --mezo \
-                    $( [ $MLW = 'true' ] && echo '--mezo_layerwise' ) \
-                    --wandb_proj $WANDB_PROJ;
-                  echo '[INFO] Finished run: $RUN_NAME';
-                  exec bash
-                "
+                      RUN_NAME="${RUN_PREFIX}${TASK}_${ARCH}_lr${LR}_eps${EPS}_mezo_LW${MLW}"
+                      echo "[INFO] Launching screen session: $RUN_NAME"
+
+                      screen -dmS "$RUN_NAME" bash -c "
+                        echo '[INFO] Starting run: $RUN_NAME';
+                        export WANDB_RUN_NAME=$RUN_NAME;
+                        python ntm_with_modern_training_runs.py \
+                          --task $TASK \
+                          --arch $ARCH \
+                          --seq_len $SEQ_LEN \
+                          --hidden_size $HS \
+                          --memory_size $MEM \
+                          --head_size $HEAD_SZ \
+                          --num_heads 1 \
+                          --batch_size $BS \
+                          --max_iters $MAX_ITERS \
+                          --log_interval $LOG_INTERVAL \
+                          --cosine_lr \
+                          --learning_rate $LR \
+                          --optimizer mezo \
+                          --mezo \
+                          --epsilon $EPS \
+                          $( [ $MLW = 'true' ] && echo '--mezo_layerwise' ) \
+                          --wandb_proj $WANDB_PROJ;
+                        echo '[INFO] Finished run: $RUN_NAME';
+                        exec bash
+                      "
+
+                      sleep 1
+                    done
+                  done
+
+                else
+                  # For Adam (standard backprop)
+                  RUN_NAME="${RUN_PREFIX}${TASK}_${ARCH}_lr${LR}_OPTadam"
+                  echo "[INFO] Launching screen session: $RUN_NAME"
+
+                  screen -dmS "$RUN_NAME" bash -c "
+                    echo '[INFO] Starting run: $RUN_NAME';
+                    export WANDB_RUN_NAME=$RUN_NAME;
+                    python ntm_with_modern_training_runs.py \
+                      --task $TASK \
+                      --arch $ARCH \
+                      --seq_len $SEQ_LEN \
+                      --hidden_size $HS \
+                      --memory_size $MEM \
+                      --head_size $HEAD_SZ \
+                      --num_heads 1 \
+                      --batch_size $BS \
+                      --max_iters $MAX_ITERS \
+                      --log_interval $LOG_INTERVAL \
+                      --cosine_lr \
+                      --learning_rate $LR \
+                      --optimizer adam \
+                      --wandb_proj $WANDB_PROJ;
+                    echo '[INFO] Finished run: $RUN_NAME';
+                    exec bash
+                  "
+
+                  sleep 1
+                fi
+
               done
-            else
-              # For Adam (standard backprop)
-              RUN_NAME="${RUN_PREFIX}${TASK}_hs${HS}_mem${MEM}_head${HEAD_SZ}_lr${LR}_OPTadam"
-              echo "[INFO] Launching screen session: $RUN_NAME"
-
-              screen -dmS "$RUN_NAME" bash -c "
-                echo '[INFO] Starting run: $RUN_NAME';
-                export WANDB_RUN_NAME=$RUN_NAME;
-                python ntm_with_modern_training_runs.py \
-                  --task $TASK \
-                  --seq_len $SEQ_LEN \
-                  --hidden_size $HS \
-                  --memory_size $MEM \
-                  --head_size $HEAD_SZ \
-                  --num_heads 1 \
-                  --cosine_lr \
-                  --batch_size $BATCH_SIZE \
-                  --max_iters $MAX_ITERS \
-                  --log_interval $LOG_INTERVAL \
-                  --learning_rate $LR \
-                  --optimizer adam \
-                  --wandb_proj $WANDB_PROJ;
-                echo '[INFO] Finished run: $RUN_NAME';
-                exec bash
-              "
-
-              
-            fi
-            sleep 5
-
+            done
           done
         done
       done
